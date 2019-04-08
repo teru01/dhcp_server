46a599bd8e4cc6fd5efcf639294f65431e1ca63e
Teruya Ono <teruya@gmail.com>
2019-04-08 21:25:58 +0900

    change buffer to ipv4

--- src/main.rs
+++ src/main.rs
@@ -22,36 +22,50 @@ enum Htype {
     ETHER = 1,
 }
 
+const HTYPE_ETHER: u8 = 1;
+const HLEN_MACADDR: u8 = 6;
 
 const DHCP_MINIMUM_SIZE: usize = 548;
 const MESSAGE_TYPE_CODE: u8 = 53;
+
+const DHCPDISCOVER: u8 = 1;
+const DHCPOFFER   : u8 = 2;
+const DHCPREQUEST : u8 = 3;
+const DHCPDECLINE : u8 = 4;
+const DHCPACK     : u8 = 5;
+const DHCPNAK     : u8 = 6;
+const DHCPRELEASE : u8 = 7;
+
+const BOOTREQUEST: u8 = 1;
+const BOOTREPLY: u8 = 2;
+
 
 // 全体で548oc以上
 struct DhcpPacket {
     op:      u8, /* 0: Message type */
     htype:   u8, /* 1: Hardware addr type */
-    hlen:    u8, /* 2: Hardware addr length */
+    hlen:    u8, /* 2: Hardware addr length MACアドレスなら6で固定 */
     hops:    u8, /* 3: agent hops from client */
     xid:     u32, /* 4: Transaction ID */
     secs:    u16, /* 8: seconds elapsed since client started to trying to boot */
     flags:   u16, /* 10: flags */
-    ciaddr:  u32, /* 12: client ip addr if already in use */
-    yiaddr:  u32, /* 16: client ip addr */
-    siaddr:  u32, /* 20: ip addr of next server to use in bootstrap */
-    giaddr:  u32, /* 24: ip addr of relay agent */
-    chaddr:  u128, /* 28: client hardware address */
+    ciaddr:  net::Ipv4Addr, /* 12: client ip addr 以前使っている値があるとき、それを通達する。リース延長のときなど */
+    yiaddr:  net::Ipv4Addr, /* 16: client ip addr */
+    siaddr:  net::Ipv4Addr, /* 20: ip addr of next server to use in bootstrap */
+    giaddr:  net::Ipv4Addr, /* 24: ip addr of relay agent */
+    chaddr:  datalink::MacAddr, /* 28: client hardware address */
     sname:   [u8; 64], /* 44: optional server host name */
     file:    [u8; 128], /* 108: boot file name */
     options: Vec<u8> /* 236: optionがどの用途に使われるのか? minで312 */
 }
 
-// fn create_ip(n: u32) -> [u8; 4]{
-//     let buf = [0u8; 4];
-//     for i in 0..4 {
-//         buf[i] = n >> (8 * (3 - i)) as u8 & 0xff;
-//     }
-//     buf
-// }
+fn create_ip(buf: &[u8]) -> net::Ipv4Addr {
+    net::Ipv4Addr::new(buf[0], buf[1], buf[2], buf[3])
+}
+
+fn create_macaddr(buf: &[u8]) -> datalink::MacAddr {
+    datalink::MacAddr::new(buf[0], buf[1], buf[2], buf[3], buf[4], buf[5])
+}
 
 impl DhcpPacket {
     fn new(buf: &[u8]) -> Option<DhcpPacket>{
@@ -63,21 +77,16 @@ impl DhcpPacket {
             xid:    NetworkEndian::read_u32(&buf[4..8]),
             secs:   NetworkEndian::read_u16(&buf[8..10]), /* 8: seconds elapsed since client started to trying to boot */
             flags:  NetworkEndian::read_u16(&buf[10..12]), /* 10: flags */
-            ciaddr: NetworkEndian::read_u32(&buf[12..16]), /* 12: client ip addr if already in use */
-            yiaddr: NetworkEndian::read_u32(&buf[16..20]), /* 16: client ip addr */
-            siaddr: NetworkEndian::read_u32(&buf[20..24]), /* 20: ip addr of next server to use in bootstrap */
-            giaddr: NetworkEndian::read_u32(&buf[24..28]), /* 24: ip addr of relay agent */
-            chaddr: NetworkEndian::read_u128(&buf[28..44]), /* 28: client hardware address */
+            ciaddr: create_ip(&buf[12..16]), /* 12: client ip addr if already in use */
+            yiaddr: create_ip(&buf[16..20]), /* 16: client ip addr */
+            siaddr: create_ip(&buf[20..24]), /* 20: ip addr of next server to use in bootstrap */
+            giaddr: create_ip(&buf[24..28]), /* 24: ip addr of relay agent */
+            chaddr: create_macaddr(&buf[28..44]), /* 28: client hardware address */
             sname:   [0u8; 64], /* 44: optional server host name */
             file:    [0u8; 128], /* 108: boot file name */
             options: buf[236..].to_vec()
         };
         Some(packet)
-    }
-
-
-    fn set_op(&mut self, op: u8) {
-        self.op = op;
     }
 
     //optionはcode, length, bufferの順に並んでいる
